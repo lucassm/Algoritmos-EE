12a83e2242d5c5b94de80bc3dd2c4c8551050a53
lucassm <lucas.silveiramelo@gmail.com>
2015-02-26 23:13:45 -0300

    inclusao do arquivo setup.py no repositorio

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name = 'rede',
+      version = '1.0',
+      description = 'Funcionalidades para representacao da rede eletrica',
+      author = 'Lucas S Melo',
+      py_modules = ['rede', 'rnp', 'util'],
+      )
