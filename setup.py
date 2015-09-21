8e2a51ea01d23e7b6c6b3f54c81bb72e497debc2
Lucas Melo <lucas.silveiramelo@gmail.com>
2015-09-21 14:29:46 -0300

    Modificacoes no arquivo setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name = 'rede',
       version = '1.0',
-      description = 'Funcionalidades para representacao da rede eletrica',
+      description = 'Funcionalidades para representacao da rede eletrica utilizando grafos com representacao no-profundidade',
+      install_requires = ['numpy==1.8.2', 'terminaltables'],
       author = 'Lucas S Melo',
-      py_modules = ['rede', 'rnp', 'util'],
+      packages = find_packages(),
       )
