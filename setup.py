a49f4e8bba5e863b16c09b9c3e9438e158992c3d
lucassm <lucas.silveiramelo@gmail.com>
2015-11-13 15:38:51 -0300

    atualizacao de pacotes

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
-from setuptools import setup, find_packages
-
 setup(name = 'rede',
-      version = '1.0',
+      version = '1.2',
       description = 'Funcionalidades para representacao da rede eletrica utilizando grafos com representacao no-profundidade',
       install_requires = ['numpy==1.8.2', 'terminaltables'],
       author = 'Lucas S Melo',
